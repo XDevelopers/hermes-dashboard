14da4d8c8c1f9135877116c5f7a6ff2aca6e7b48
rarestoma <tomairares@gmail.com>
2017-08-29 14:53:46 +0300

    plugin
    added the plugin

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -1,6 +1,54 @@
 type = ['','info','success','warning','danger'];
     	
 var seq = 0, delays = 80, durations = 500;
+
+$().ready(function(){
+
+  $pageheader = $('.page-header');
+  $pageheader_img_container = $pageheader.find('.page-header-background');
+
+  $('.fixed-plugin a').click(function(event){
+      // Alex if we click on switch, stop propagation of the event, so the dropdown will not be hide, otherwise we set the  section active
+        if($(this).hasClass('switch-trigger')){
+            if(event.stopPropagation){
+                event.stopPropagation();
+            }
+            else if(window.event){
+               window.event.cancelBubble = true;
+            }
+        }
+    });
+
+     $('.fixed-plugin .badge').click(function(){
+
+        $(this).siblings().removeClass('active');
+        $(this).addClass('active');
+
+        var new_color = $(this).data('color');
+
+        $pageheader.attr('data-color',new_color);
+
+    });
+
+    $('.fixed-plugin .img-holder').click(function(){
+
+        $(this).parent('li').siblings().removeClass('active');
+        $(this).parent('li').addClass('active');
+
+
+        var new_image = $(this).find("img").attr('src');
+
+        $pageheader_img_container.fadeOut('fast', function(){
+           $pageheader_img_container.css('background-image','url("' + new_image + '")');
+           $pageheader_img_container.fadeIn('fast');
+        });
+
+        if($sidebar_responsive.length != 0){
+            $sidebar_responsive.css('background-image','url("' + new_image + '")');
+        }
+    });
+
+});
 demo = {
    initPickColor: function(){
         $('.pick-class-label').click(function(){
